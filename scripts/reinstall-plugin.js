9e7bce2978839a8fa993eaa7e69b32b902ee0361
Josh Noel <obsidize@gmail.com>
2021-02-19 21:03:04 -0600

    plugin reinstall script tweaks

--- scripts/reinstall-plugin.js
+++ scripts/reinstall-plugin.js
@@ -1,6 +1,6 @@
 const { pluginAdd, pluginRemove } = require('./cordova-util');
 
-pluginRemove('cordova-plugin-geolocation-tests');
-pluginRemove('cordova-plugin-geolocation');
-pluginAdd('../cordova-plugin-geolocation');
-pluginAdd('./plugins/cordova-plugin-geolocation/tests');
+pluginRemove('cordova-plugin-geolocation-tests --nosave');
+pluginRemove('cordova-plugin-geolocation --nosave');
+pluginAdd('../cordova-plugin-geolocation --nosave');
+pluginAdd('./plugins/cordova-plugin-geolocation/tests --nosave');
